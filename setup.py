0596fe238937f0d0119918b85b92e5fb38f01af3
Kevin D. Jones, Ph.D <kevin.d.jones@outlook.com>
2017-07-15 15:17:25 -0400

    Added a setup.py file.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(name='endurancemodelobfuscator',
+      version='0.0.1',
+      description="An Endurance SDK library for network model obfuscation.",
+      url='http://www.gridwidgetlabs.com/products/endurance-sdk',
+      author='Kevin D. Jones, Ph.D.',
+      author_email='kevin.d.jones@gridwidgetlabs.com',
+      license='MIT',
+      install_requires=[
+      ],
+    #   entry_points={
+    #     'console_scripts': [
+    #         'endurance-admin=endurance.management.cli:main',
+    #     ],
+    # },
+      zip_safe=False)
